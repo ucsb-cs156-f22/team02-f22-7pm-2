8bc0c3a5996834314374ca808607954d8a2eddef
EthanEpp <eepp@ucsb.edu>
2022-10-27 17:37:40 -0700

    ee - MenuItem repository and entity added with id fix

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -1,7 +1,9 @@
 package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,10 @@ import lombok.Builder;
 
 @Entity(name = "ucsbdiningcommonsmenuitem")
 public class UCSBDiningCommonsMenuItem {
-    @Id
-    private String diningCommonsName;
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String diningCommonsCode;
     private String name;
     private String station;
 }
